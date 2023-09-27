05e9194394bacdc18ac88c263cb65e9b5e2dca57
Critiek <jumbra99@gmail.com>
2023-09-28 00:48:07 +0930

    changed to jedi-language-server

--- config.lua
+++ config.lua
@@ -33,9 +33,16 @@ lvim.plugins = {
   config = function()
     require("nvim-ts-autotag").setup()
   end,
-}
+  }
 }
 
 lvim.colorscheme = "catppuccin-mocha"
 
 vim.opt.relativenumber = true -- relative line numbers
+
+-- add `pyright` to `skipped_servers` list
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+-- remove `jedi_language_server` from `skipped_servers` list
+lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+  return server ~= "jedi_language_server"
+end, lvim.lsp.automatic_configuration.skipped_servers)
